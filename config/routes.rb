44dc6db9e1127e372cbebfa830152f8cffd00e32
shotaogw <shotaogw@gmail.com>
2021-01-11 20:05:24 +0900

    deviseに関するビューファイルの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
-  devise_for :users
+  devise_for :users, controllers: { registrations: 'users/registrations',
+                                    sessions: 'users/sessions' }
   resources :users, only: :index
   root to: "users#index"
 end
