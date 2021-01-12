0b0509dec2f47f8db27a98b54b67ffd03ad9683d
shotaogw <shotaogw@gmail.com>
2021-01-12 16:56:46 +0900

    マイページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users, controllers: { registrations: 'users/registrations',
                                     sessions: 'users/sessions' }
-  resources :users, only: :index
+  resources :users, only: [:index, :show]
   root to: "users#index"
 end
