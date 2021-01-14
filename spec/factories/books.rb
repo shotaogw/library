a59669aaeba46f6dd87cf9890dc562b58f17ca56
shotaogw <shotaogw@gmail.com>
2021-01-14 23:54:30 +0900

    Bookモデル単体テスト

--- spec/factories/books.rb
+++ spec/factories/books.rb
@@ -1,4 +1,7 @@
 FactoryBot.define do
   factory :book do
+    title { Faker::Lorem.words }
+    genre_id { 7 }
+    association :user
   end
 end
--- spec/models/book_spec.rb
+++ spec/models/book_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @book = FactoryBot.build(:book)
+  end
+
+  describe '新規ブック作成' do
+    context 'ブックが作成できるとき' do
+      it '必要な情報を適切に入力するとブック作成ができる' do
+        expect(@book).to be_valid
+      end
+    end
+
+    context 'ブック作成ができないとき' do
+      it 'titleが空では作成できない' do
+        @book.title = ''
+        @book.valid?
+        expect(@book.errors.full_messages).to include('タイトルを入力してください')
+      end
+      it 'genre_idの情報がないと作成できない' do
+        @book.genre_id = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include('ジャンルを選択してください')
+      end
+      it 'genre_idが1では作成できない' do
+        @book.genre_id = 1
+        @book.valid?
+        expect(@book.errors.full_messages).to include('ジャンルを選択してください')
+      end
+      it 'userが紐づいていないと作成できない' do
+        @book.user = nil
+        @book.valid?
+        expect(@book.errors.full_messages).to include('ユーザーを入力してください')
+      end
+    end
+  end
 end
