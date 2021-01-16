8ca2c0ceb441752f0bbccb0a8c70e2588c9d1423
shotaogw <shotaogw@gmail.com>
2021-01-16 09:34:04 +0900

    Wordモデル単体テスト

--- spec/models/word_spec.rb
+++ spec/models/word_spec.rb
@@ -1,5 +1,38 @@
 require 'rails_helper'
 
 RSpec.describe Word, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @word = FactoryBot.build(:word)
+  end
+
+  describe '新規ワード作成' do
+    context 'ワードが作成できるとき' do
+      it '必要な情報を適切に入力するとワード作成ができる' do
+        expect(@word).to be_valid
+      end
+    end
+
+    context 'ワード作成ができないとき' do
+      it 'nameが空では作成できない' do
+        @word.name = ''
+        @word.valid?
+        expect(@word.errors.full_messages).to include('単語を入力してください')
+      end
+      it 'infoが空では作成できない' do
+        @word.info = ''
+        @word.valid?
+        expect(@word.errors.full_messages).to include('定義を入力してください')
+      end
+      it 'userが紐づいていないと作成できない' do
+        @word.user = nil
+        @word.valid?
+        expect(@word.errors.full_messages).to include('ユーザーを入力してください')
+      end
+      it 'bookが紐づいていないと作成できない' do
+        @word.book = nil
+        @word.valid?
+        expect(@word.errors.full_messages).to include('ブックを入力してください')
+      end
+    end
+  end
 end
