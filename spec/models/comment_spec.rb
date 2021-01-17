93e95f6dc405365fe9e2c775e1313ffc20d14050
shotaogw <shotaogw@gmail.com>
2021-01-17 09:31:21 +0900

    Commentモデル単体テスト

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Comment, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before do
+    @comment = FactoryBot.build(:comment)
+  end
+
+  describe 'ブックへのコメント' do
+    context 'コメントできるとき' do
+      it '必要な情報を適切に入力するとコメントができる' do
+        expect(@comment).to be_valid
+      end
+    end
+
+    context 'コメントできないとき' do
+      it 'contentが空ではコメントできない' do
+        @comment.content = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('コメントを入力してください')
+      end
+      it 'userが紐づいていないとコメントできない' do
+        @comment.user = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('ユーザーを入力してください')
+      end
+      it 'bookが紐づいていないとコメントできない' do
+        @comment.book = nil
+        @comment.valid?
+        expect(@comment.errors.full_messages).to include('ブックを入力してください')
+      end
+    end
+  end
 end
